bbfb2ae29d9031bd2007047f5f1e434f01d9ff96
thisisnilesh <lagadnilesh5@gmail.com>
2025-11-02 22:51:17 +0530

    Remove all email functionality - no emails sent

--- landing/urls.py
+++ landing/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("signup/", views.signup, name="signup"),
     path("signup/success/", views.signup_success, name="signup_success"),
-    path("process-emails/", views.process_emails, name="process_emails"),
     path("privacy/", views.privacy, name="privacy"),
     path("terms/", views.terms, name="terms"),
 ]
